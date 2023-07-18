4409b47267e296dc54c3b09bd489385932a9d003
Szabo Gergo <szabo.gergo.bme@gmail.com>
2023-07-18 20:19:53 +0200

    Contact form working with email

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
+import smtplib
+
 import requests
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 api_endpoint = "https://api.npoint.io/6da3bd5b6bb70128aca2"
+my_email = os.environ["EMAIL"]
+password = os.environ["PASS"]
 
 all_posts_json = requests.get(url=api_endpoint).json()
 
@@ -17,9 +22,22 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route('/form-entry', methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    with smtplib.SMTP_SSL("smtp.gmail.com") as connection:
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="szabo.gergo.bme@gmail.com",
+            msg=f"Subject:New blog message received\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}")
 
 
 @app.route('/post/<blog_id>')
